c7ce4cf8763abb1e6024211c38922a1dc06e6ff0
Takumi Shikida <t.s.csmajor@gmail.com>
2025-11-29 03:07:23 +0900

    I wrote hash_table.h

--- src/hash_table.h
+++ src/hash_table.h
@@ -1,9 +1,22 @@
+#pragma once
+#include "game.h"
 #include <iostream>
-#include <fstream>
-#include <sstream>
-using namespace std;
+#include <vector>
+#include <list>
 
-int main()
-{
-	cout << "HLTB" << endl;
-}
+class HashTable{
+public:
+	HashTable(size_t bucketNum = 101);
+	void makeTable(const std::vector<Game>& games);
+	bool getAverage(const std::string& genre, double& avgHours) const;
+
+private:
+	struct Entry{
+		std::string genre;
+		double totalHours;
+		int gamesNum; // The number of games for the genre
+	};
+
+	std::vector<std::list<Entry>> table;
+	size_t hash(const std::string& key) const;
+};
